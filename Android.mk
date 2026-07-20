1af78bd33a1f8c1688b14c575991fa2d3e1443bf
JNWSG <jinwoosung@zohomail.in>
2026-07-20 17:20:40 +0000

    Track radio and MotCamera4 with Git LFS

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),fogos)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b4d7f4dca7521ed61e8f0147b972276445166ff9)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,0bdd1e2857392cfb63192a18534a44148e3a04b3)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,e814b7ced35e852dca3c2df6068ef221a1f1838e)
+$(call add-radio-file-sha1-checked,radio/dsp.img,6be03a523b4a75f993c9cf3f14537feaf6f2e57e)
+$(call add-radio-file-sha1-checked,radio/fsg.img,83b3ab01ccaa94e0246d5433e924f8644183de13)
+$(call add-radio-file-sha1-checked,radio/hyp.img,502ad663c227d2aa28850bfd648be433a18b5dce)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,83f18f4ba0f5a39479c739f640b1f4d799deceb4)
+$(call add-radio-file-sha1-checked,radio/logo.img,0acaf57bca97f7046f8849d8ff8f27324d070f47)
+$(call add-radio-file-sha1-checked,radio/modem.img,b21bac12ce3408056d4efd485d1155326f2ef06d)
+$(call add-radio-file-sha1-checked,radio/prov.img,ff405d516f64fb894ce48469c8ac44a88c70a76c)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,30501e1d7e84bc052535a0ea1fc68ebfde0e6878)
+$(call add-radio-file-sha1-checked,radio/rpm.img,99fba5b169db459f3a396a28562de658d44134d8)
+$(call add-radio-file-sha1-checked,radio/storsec.img,7d3612ccf6b27bda393faeba446294228a1d2ee8)
+$(call add-radio-file-sha1-checked,radio/tz.img,700edb543d5ac647a4bfdccd433817dc0ac6e35d)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,b3ee913179fa2e8599e7c2ab57b815f4b2e8382b)
+$(call add-radio-file-sha1-checked,radio/xbl.img,c41a368baeaf44feb2718ae9792a7c2bfcab6537)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,00eea88e2663f7d8a46cede96c27f30881247b12)
+
+endif
